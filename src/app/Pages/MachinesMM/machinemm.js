45e6c26379f6957c8a4721513fe273cd2e057eed
Kiruthika-sys <kiruthikamanoharan.ml@gmail.com>
2025-09-25 17:00:40 +0530

    seconds value correction

--- src/app/Pages/MachinesMM/machinemm.js
+++ src/app/Pages/MachinesMM/machinemm.js
@@ -868,7 +868,7 @@ useEffect(() => {
 function formatTime(seconds) {
   const hrs = Math.floor(seconds / 3600);
   const mins = Math.floor((seconds % 3600) / 60);
-  const secs = seconds % 60;
+  const secs = Math.floor(seconds % 60);
   return `${hrs}h ${mins}m ${secs}s`;
 }
 
