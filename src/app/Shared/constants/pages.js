b235e291e248700c7c1dd10e289aabb862043e8e
yogesans <yogesan.s@yantra24x7.com>
2025-11-11 15:39:41 +0530

    page updated

--- src/app/Shared/constants/pages.js
+++ src/app/Shared/constants/pages.js
@@ -3,17 +3,18 @@ export const PAGE_LIST = [
     { label: "Company", value: "company" },
     { label: "Machine MM", value: "machinemm" },
     { label: "Device OEE", value: "deviceoee" },
-    { label: "Operation Analytics", value: "analytics" },
-    { label: "Component Analytics", value: "production-analysis" },
+    // { label: "Operation Analytics", value: "analytics" },
+    // { label: "Component Analytics", value: "production-analysis" },
     { label: "Operator Details", value: "operator-details" },
     { label: "Machines", value: "machines" },
     { label: "Machine Group", value: "machines-group" },
     { label: "Shift Registration", value: "shift-registration" },
-    // { label: "Operator Registration", value: "operator-registration" },
     { label: "User Registration", value: "user-registration" },
     { label: "Component Registration", value: "component-registration" },
     { label: "Reason Registration", value: "reason-registration" },
 ];
+
+    // { label: "Operator Registration", value: "operator-registration" },
 
 //   { label: "Andon Dashboard", value: "andon-dashboard" },
 //   { label: "Shift", value: "shift" },
