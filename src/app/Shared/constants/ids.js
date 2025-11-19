c31f7d16bcf172be3739f39a1c422c3d783e4171
yogesans <yogesan.s@yantra24x7.com>
2025-11-19 12:07:16 +0530

    reverted operator id change

--- src/app/Shared/constants/ids.js
+++ src/app/Shared/constants/ids.js
@@ -3,8 +3,7 @@
 export const CUSTOMER_IDS = {
   ATECH: "05391cd0-3d37-11f0-b2f2-c1aac5b33cac",
   PMI: "690d2210-8a3a-11f0-a3ac-9b534c07af2b",
-  HITECH: "84fd0f90-9ac4-11f0-a20b-db03187ae4d2",
-  OFFICE_USER: "b9446ad0-9526-11f0-9f03-851f0943b960",
+  HITECH: "84fd0f90-9ac4-11f0-a20b-db03187ae4d2"
 };
 
 //local
