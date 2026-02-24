801859e3b351ead3e420ce2db319b0977359b37e
yogesans <yogesan.s@yantra24x7.com>
2026-02-24 17:21:24 +0530

    pages added

--- src/app/Shared/constants/pages.js
+++ src/app/Shared/constants/pages.js
@@ -15,7 +15,6 @@ export const PAGE_LIST = [
     { label: "Group Registration", value: "group" },
     { label: "Component Registration", value: "component-registration" },
     { label: "Reason Registration", value: "reason-registration" },
-    { label: "Group", value: "group" },
     { label: "Settings", value: "settings" },
     { label: "Production Metrics", value: "production-metrics" },
 ];
